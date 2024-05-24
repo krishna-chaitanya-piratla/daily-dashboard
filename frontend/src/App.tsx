b2748c4fbf6120bc22d6b29aec7aeea6264cbea4
krishna-chaitanya-piratla <krishnachaitanya.piratla@gmail.com>
2024-05-24 14:23:42 -0400

    Add react-helmet types; Change global font family to Lato

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import DateTimeLocation from './components/DateTimeLocation';
 import { GlobalStyles, Header } from './styled-components/GlobalStyles';
+import { Helmet } from 'react-helmet';
+
 
 const App: React.FC = () => {
   return (
     <>
+    <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+    </Helmet>
     <GlobalStyles />
     <Header>
       <DateTimeLocation />
