8950868c525a37f2cf6beb0b8f6f2f1f45703ee9
krishna-chaitanya-piratla <krishnachaitanya.piratla@gmail.com>
2024-05-27 07:32:14 -0400

    Initialize backend module to handle todos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TodosModule } from './todos/todos.module';
 
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }),],
+  }), TodosModule,],
   controllers: [AppController],
   providers: [AppService],
 })
