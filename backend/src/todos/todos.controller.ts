3184d1d3d01b80bf9a485bc0034edc052ecadf2f
krishna-chaitanya-piratla <krishnachaitanya.piratla@gmail.com>
2024-05-27 07:35:03 -0400

    Add the todos handling functionality to the todos module

--- backend/src/todos/todos.controller.ts
+++ backend/src/todos/todos.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+// src/todo/todo.controller.ts
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private readonly todoService: TodosService) {}
+
+  @Get()
+  getAllTodos() {
+    return this.todoService.getAllTodos();
+  }
+
+  @Post()
+  addTodo(@Body() todo: any) {
+    return this.todoService.addTodo(todo);
+  }
+
+  @Put(':index')
+  updateTodo(@Param('index') index: number, @Body() todo: any) {
+    return this.todoService.updateTodo(index, todo);
+  }
+
+  @Delete(':index')
+  deleteTodo(@Param('index') index: number) {
+    return this.todoService.deleteTodo(index);
+  }
+
+  @Delete()
+  clearTodos() {
+    this.todoService.clearTodos();
+  }
+}
--- backend/src/todos/todos.service.ts
+++ backend/src/todos/todos.service.ts
@@ -1,4 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const TODO_FILE_PATH = path.resolve(__dirname, '../../data/todos.json');
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  private todos: any[] = [];
+
+  constructor() {
+    this.loadTodos();
+  }
+
+  private loadTodos() {
+    if (fs.existsSync(TODO_FILE_PATH)) {
+      const data = fs.readFileSync(TODO_FILE_PATH, 'utf8');
+      this.todos = JSON.parse(data) || [];
+    } else {
+      this.todos = [];
+    }
+  }
+
+  private saveTodos() {
+    fs.writeFileSync(TODO_FILE_PATH, JSON.stringify(this.todos, null, 2), 'utf8');
+  }
+
+  getAllTodos() {
+    return this.todos;
+  }
+
+  addTodo(todo: any) {
+    this.todos.push(todo);
+    this.saveTodos();
+    return todo;
+  }
+
+  updateTodo(index: number, todo: any) {
+    if (index >= 0 && index < this.todos.length) {
+      this.todos[index] = todo;
+      this.saveTodos();
+      return todo;
+    }
+    return null;
+  }
+
+  deleteTodo(index: number) {
+    if (index >= 0 && index < this.todos.length) {
+      const deleted = this.todos.splice(index, 1);
+      this.saveTodos();
+      return deleted[0];
+    }
+    return null;
+  }
+
+  clearTodos() {
+    this.todos = [];
+    this.saveTodos();
+  }
+}
