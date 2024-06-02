8674cc8c7fb6fa036644bc8e43504d441ad3f1b0
krishna-chaitanya-piratla <krishnachaitanya.piratla@gmail.com>
2024-06-02 19:38:15 -0400

    Re-enable Sidebar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -58,7 +58,7 @@ const App: React.FC = () => {
       </Helmet>
       <GlobalStyles />
       <Background type="solid">
-        {/* <Sidebar addTodoList={addTodoList} isOpen={isSidebarOpen} onClose={closeSidebar} onOpen={openSidebar} /> */}
+        <Sidebar addTodoList={addTodoList} isOpen={isSidebarOpen} onClose={closeSidebar} onOpen={openSidebar} />
         <AppContainer>
           <Header>
             <LocationWeather />
