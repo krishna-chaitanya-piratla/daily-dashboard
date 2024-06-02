43e1105ac46d320a4591bc5027debcb3aa834d26
krishna-chaitanya-piratla <krishnachaitanya.piratla@gmail.com>
2024-06-02 08:30:24 -0400

    Make 'Add a new to-do' placeholder text always visible

--- frontend/src/components/TodoList/TodoInput.tsx
+++ frontend/src/components/TodoList/TodoInput.tsx
@@ -1,30 +1,26 @@
-import React, { useState } from "react";
-import { StyledTodoInput, StyledTodoInputContainer } from '../../styled-components/TodoList/TodoInput';
+import React from "react";
+import { StyledTodoInput } from "../../styled-components/TodoList/TodoInput";
 
 interface TodoInputProps {
-    type: 'text';
-    value: string;
-    onChange: (e: any) => void;
-    onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-    placeholder: string;
-    disabled: boolean;
-  }
+  type: 'text';
+  value: string;
+  onChange: (e: any) => void;
+  onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  disabled: boolean;
+}
 
 const TodoInput: React.FC<TodoInputProps> = ({ type, value, onChange, onKeyDown, placeholder, disabled }) => {
-    const [isHovered, setIsHovered] = useState(false);
-
-    return (
-        <StyledTodoInput 
-            type={type} 
-            value={value} 
-            onChange={onChange} 
-            onKeyDown={onKeyDown} 
-            placeholder={isHovered ? placeholder : ''}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            disabled={disabled}
-        />
-    )
+  return (
+    <StyledTodoInput 
+      type={type} 
+      value={value} 
+      onChange={onChange} 
+      onKeyDown={onKeyDown} 
+      placeholder={placeholder}
+      disabled={disabled}
+    />
+  );
 };
 
 export default TodoInput;
