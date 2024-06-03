c10f73efce248d65fa7158109faf9ed53619b406
krishna-chaitanya-piratla <krishnachaitanya.piratla@gmail.com>
2024-06-03 09:13:26 -0400

    Add display options to the radio buttons in BackgroundSettings

--- frontend/src/components/Sidebar/BackgroundSettings.tsx
+++ frontend/src/components/Sidebar/BackgroundSettings.tsx
@@ -1,20 +1,47 @@
-import React from 'react';
-import { BackgroundSettingsContainer, RadioButtonContainer } from '../../styled-components/Sidebar/BackgroundSettings';
+import React, { useState } from 'react';
+import { BackgroundSettingsContainer, RadioButtonContainer, ColorBoxContainer, ColorBox, TextInput } from '../../styled-components/Sidebar/BackgroundSettings';
 
 const BackgroundSettings: React.FC = () => {
+  const [selectedBackground, setSelectedBackground] = useState<string>('solid');
+
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedBackground(event.target.value);
+  };
+
   return (
     <BackgroundSettingsContainer>
       <h3>Background</h3>
       <RadioButtonContainer>
         <label>
-          <input type="radio" name="background" value="solid" />
+          <input
+            type="radio"
+            name="background"
+            value="solid"
+            checked={selectedBackground === 'solid'}
+            onChange={handleRadioChange}
+          />
           Solid
         </label>
         <label>
-          <input type="radio" name="background" value="unsplash" />
+          <input
+            type="radio"
+            name="background"
+            value="unsplash"
+            checked={selectedBackground === 'unsplash'}
+            onChange={handleRadioChange}
+          />
           Unsplash
         </label>
       </RadioButtonContainer>
+      {selectedBackground === 'solid' ? (
+        <ColorBoxContainer>
+          <ColorBox color="red" />
+          <ColorBox color="blue" />
+          <ColorBox color="green" />
+        </ColorBoxContainer>
+      ) : (
+        <TextInput type="text" placeholder="Enter search query" />
+      )}
     </BackgroundSettingsContainer>
   );
 };
--- frontend/src/styled-components/Sidebar/BackgroundSettings.tsx
+++ frontend/src/styled-components/Sidebar/BackgroundSettings.tsx
@@ -15,3 +15,22 @@ export const RadioButtonContainer = styled.div`
     gap: 0.5rem;
   }
 `;
+
+export const ColorBoxContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  margin-top: 0.5rem;
+`;
+
+export const ColorBox = styled.div<{ color: string }>`
+  width: 1rem;
+  height: 1rem;
+  background-color: ${(props) => props.color};
+`;
+
+export const TextInput = styled.input`
+  width: 100%;
+  padding: 0.5rem;
+  margin-top: 0.5rem;
+  box-sizing: border-box;
+`;
